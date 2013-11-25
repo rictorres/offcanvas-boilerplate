b1f478a8d9a8c134d0d8906d76aa44ad8b03302b
Ricardo Torres <ricardo@rictorres.com.br>
2013-11-25 13:50:33 -0200

    Add off-canvas functionality to the sidebar

--- dev/assets/js/app/main.js
+++ dev/assets/js/app/main.js
@@ -2,4 +2,15 @@
 
   $('html').removeClass('no-js');
 
+  $('.btn-sidebar-toggle').on('click', function() {
+    var target = $(this).data('target'),
+        fx     = $(this).data('fx'),
+        dir    = $(this).data('dir');
+
+    $('.' + target).toggleClass('sidebar-active'); // toggle active class on the sidebar
+    $('.container').toggleClass('sidebar-open'); // toggle open class on the container
+    $('.container').toggleClass('sidebar-' + fx);
+    $('.container').toggleClass('sidebar-' + dir);
+  });
+
 }(this, this.document, this.jQuery));
