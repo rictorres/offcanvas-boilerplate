1e14b404f1f14b43d3ab629fdc3429f1624250df
Ricardo Torres <ricardo@rictorres.com.br>
2013-11-26 03:08:34 -0200

    Major updates. Closes #2, fixes #3 and resolves #4

--- dev/assets/js/app/main.js
+++ dev/assets/js/app/main.js
@@ -1,19 +1,44 @@
 ;(function(window, document, $) {
 
+  var $container = $('.container'),
+      $body      = $('body');
+
+  function closeSidebar(event) {
+    if ( !$(event.target).hasClass('sidebar') ) {
+
+      $container.removeClass('has-sidebar-toright');
+      $container.removeClass('has-sidebar-toleft');
+      $container.removeClass('has-sidebar-push');
+      $container.removeClass('has-sidebar-slideover');
+      $('.btn-sidebar-toggle').removeClass('is-active');
+      $('.sidebar').removeClass('is-active');
+      setTimeout( function() {
+        $body.removeClass('has-sidebar-open');
+      }, 400);
+      $container.off('click', closeSidebar);
+    }
+  }
+
   $('.btn-sidebar-toggle').on('click', function(event) {
     event.preventDefault();
 
     var $this       = $(this),
         target      = $this.data('target'),
         fx          = $this.data('fx'),
-        dir         = $this.data('dir'),
-        $container  = $('.container');
+        dir         = $this.data('dir');
 
     $('[data-target=' + target + ']').toggleClass('is-active');
     $('.' + target).toggleClass('is-active');
-    $container.toggleClass('has-sidebar-open');
+    $body.toggleClass('has-sidebar-open');
     $container.toggleClass('has-sidebar-' + fx);
     $container.toggleClass('has-sidebar-' + dir);
+
+    setTimeout( function() {
+      if ( $body.hasClass('has-sidebar-open') ) {
+        $container.on('click', closeSidebar);
+      }
+    }, 25);
+
   });
 
 }(this, this.document, this.jQuery));
